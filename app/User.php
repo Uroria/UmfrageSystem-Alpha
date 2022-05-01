842aeacc23ada7e5c24c0231d82cf28435110d88
RetteDasPlanetGit <74604261+RetteDasPlanetGit@users.noreply.github.com>
2022-05-01 18:26:36 +0200

    Rework 0.1

---
+++ app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Webpatser\Uuid\Uuid;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Wrapper for the uuid.
+     */
+    public function save(array $options = [])
+    {
+        $this->uuid = Uuid::generate(4);
+
+        return parent::save($options);
+    }
+}
